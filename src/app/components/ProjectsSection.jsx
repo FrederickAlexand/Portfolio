9083bf335222863dfa346a5a94aeea112a75f4c1
FrederickAlex17 <frederick.alexander@student.umn.ac.id>
2024-09-03 18:20:06 +0700

    tambah page about,navbar, hero, projects

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,112 @@
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Decorative fish E-commerce Marketplace",
+    description: "Our Decorative Fish E-commerce Marketplace is a one-stop shop for enthusiasts and hobbyists to explore, buy, and learn about a diverse range of exotic and decorative fish. ",
+    image: "/images/projects/1.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Halal Tourism and Food E-commerce Recomendation Webiste",
+    description: "This website serves as a comprehensive platform for Muslim travelers and food enthusiasts seeking halal-certified options. It provides recommendations for halal-friendly tourist destinations, accommodations, and activities, ensuring a culturally respectful travel experience. Additionally, the website offers a curated selection of halal food products and dining options",
+    image: "/images/projects/2.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "E-commerce Application",
+    description: "Project 3 description",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Food Ordering Application",
+    description: "Project 4 description",
+    image: "/images/projects/4.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "React Firebase Template",
+    description: "Project 5 description",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Full-Stack Roadmap",
+    description: "Project 6 description",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={() => handleTagChange("All")}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={() => handleTagChange("Web")}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={() => handleTagChange("Mobile")}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
