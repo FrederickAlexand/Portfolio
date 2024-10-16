7e8b67a038f8a3c41a4c790225cdcf32f30ed256
FrederickAlex17 <frederick.alexander@student.umn.ac.id>
2024-10-17 03:36:51 +0700

    ok

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -187,7 +187,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg leading-relaxed mb-6">
-            I'm a{" "}
+            I&apos;m a{" "}
             <span className="text-yellow-400 font-bold">
               passionate Full-Stack Developer
             </span>{" "}
